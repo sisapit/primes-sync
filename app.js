8bbe7e1f934bd2d7d9cc67965762aa0ead832128
Sebastian <seb.scheffler@gmx.de>
2016-01-13 10:06:13 +0100

    Added callback comments. Added 'listening' event handler.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var http = require('http'); var url = require('url');
 
 http.createServer(function handler(req, res) {
+
+    // -- Event "request" handler
+
+    // Emitted each time there is a request. Note that there may be multiple requests per connection (in the case 
+    // of keep-alive connections). request is an instance of http.IncomingMessage and response is an instance of 
+    // http.ServerResponse.
+
     var q = url.parse(req.url, true);
     var max = q.query.max;
     if (! max) {
@@ -23,14 +30,19 @@ http.createServer(function handler(req, res) {
         }
         res.end(primes + " primes found between 0 and " + max);
     }
-}).listen(8080, '127.0.0.1');
+}).listen(8080, '127.0.0.1', function() {
 
-console.log('Server running at http://127.0.0.1:8080/');
-console.log('');
-console.log('Example 1: http://127.0.0.1:8080');
-console.log('Example 2: http://127.0.0.1:8080?max=100');
-console.log('Example 3: http://127.0.0.1:8080?max=10000');
-console.log('Example 4: http://127.0.0.1:8080?max=1000000');
-console.log('Example 5: http://127.0.0.1:8080?max=100');
-console.log('Example 6: http://127.0.0.1:8080?max=100');
-console.log('Example 7: http://127.0.0.1:8080?max=100');
+    // -- Event "listening" handler
+
+    console.log('Server listening on 127.0.0.1:8080...');
+
+    console.log('Example 1: http://127.0.0.1:8080');
+    console.log('Example 2: http://127.0.0.1:8080?max=100');
+    console.log('Example 3: http://127.0.0.1:8080?max=10000');
+    console.log('Example 4: http://127.0.0.1:8080?max=1000000');
+    console.log('Example 5: http://127.0.0.1:8080?max=100');
+    console.log('Example 6: http://127.0.0.1:8080?max=100');
+    console.log('Example 7: http://127.0.0.1:8080?max=100');
+});
+
+console.log('Starting server...');
